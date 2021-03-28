aedb47df78ef3c08b90b833f334e3e30edab9e9c
yanpengyin <1092957495@qq.com>
2021-03-28 12:44:08 +0800

    spring boot初识

---
+++ springBoot-helloWorld/src/main/java/com/yyp/springBoot/Application.java
@@ -0,0 +1,26 @@
+package com.yyp.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springBoot项目启动入口，由于内嵌tomcat所以直接启动此类即可
+ *
+ * SpringBootApplication：用于标记此类为springboot启动类
+ * 启动类需要放到所有需要扫描进spring容器中类的父目录中
+ * 因为默认将启动类所在的路径当作包扫面的基础路径
+ *   SpringBoot的启动类（入口）
+ *   @Configuration spring.xml 也是配置类
+ *   @ComponentScan = <context:component-scan basePackages="cn.tulingxueyuan"></context:component-scan>
+ *      Spring底层在解析配置类， 回去解析@ComponentScan,读取basePackages，
+ *       如果没有读取到，会将当前配置类所在的包当做扫描包
+ *
+ *  位置： 最好放在需要扫描包的根目录、或者说放在所有Bean的顶层目录中
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
